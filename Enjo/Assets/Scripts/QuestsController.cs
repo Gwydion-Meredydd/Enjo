ea724061adfbdfb4f64a1863081e4d298a59390a
Gwydion Meredydd <49554392+Gwydion-Meredydd@users.noreply.github.com>
2020-02-10 16:23:47 +0000

    QuestController
    Quest Controlelr added with list inside list

---
+++ Enjo/Assets/Scripts/QuestsController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class QuestsController : MonoBehaviour
+{
+    public int PickUpAmmount;
+    public int HasPickedUpAmmount;
+
+    public int QuestNumber;
+    public QuestList QuestList = new QuestList();
+
+    public void Start()
+    {
+        NewQuest();
+    }
+    public void NewQuest()
+    {
+        for (int i = 0; i < QuestList.Length; i++)
+        {
+
+        }
+        QuestNumber++;
+    }
+    public void pickup()
+    {
+
+    }
+
+}
+[System.Serializable]
+public class Quests
+{
+    public string Name;
+    public List<GameObject> QuestItems;
+}
+
+[System.Serializable]
+public class QuestList
+{
+    public List<Quests> Quests;
+}
+
