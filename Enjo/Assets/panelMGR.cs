0efe9372073fc5f39731897600223db89ffde933
JessGT <49551896+JessGT@users.noreply.github.com>
2020-02-23 01:20:03 +0000

    Starting Manga Anim Scene
    Starting an animated manga sequence to introduce the game

---
+++ Enjo/Assets/panelMGR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class panelMGR : MonoBehaviour
+{
+    public GameObject panelDisable;
+    public GameObject panelEnable;
+   
+
+    void Update()
+    {
+      if  (Input.GetKeyDown(KeyCode.Space))
+        {
+            panelDisable.SetActive(false);
+            Debug.Log("Disabled!");
+        }
+
+      else
+        {
+            panelEnable.SetActive(true);
+            Debug.Log("Enabled!");
+        }
+    }
+}
