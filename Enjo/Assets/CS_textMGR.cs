efc43abaf80db7b9d8510e2063c19df847d0c0db
Jess Roughton <49551896+JessGT@users.noreply.github.com>
2020-02-25 16:18:52 +0000

    Manga Intro Dev #2
    Added to manga intro scene

---
+++ Enjo/Assets/CS_textMGR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_textMGR : MonoBehaviour
+{
+
+    [Header("Enabling Text")]
+    public GameObject textDisable;
+    public GameObject textEnable;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            textDisable.SetActive(false);
+            Debug.Log("Text disabled!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            textEnable.SetActive(true);
+            Debug.Log("Text Enabled!");
+        }
+    }
+}
--- Enjo/Assets/panelMGR.cs
+++ Enjo/Assets/panelMGR.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class panelMGR : MonoBehaviour
 {
+    [Header("Enabling Panels")]
     public GameObject panelDisable;
     public GameObject panelEnable;
-   
 
+  
     void Update()
     {
       if  (Input.GetKeyDown(KeyCode.Space))
